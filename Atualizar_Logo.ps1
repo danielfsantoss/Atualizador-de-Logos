f22dfaf49e1d75f174db2e1fc783a7565793a54a
Daniel Figueiredo <58513369+danielfsantoss@users.noreply.github.com>
2023-05-17 15:05:46 -0300

    Update Atualizar_Logo.ps1

--- Atualizar_Logo.ps1
+++ Atualizar_Logo.ps1
@@ -2,8 +2,8 @@
 # Github: https://github.com/danielfsantoss
 
 $empresa = 'Empresa.bmp'
-$list_arq = @("imp_logo.bmp","Venda.bmp","Venda2.bmp") # Altere para os arquivos de logo necess·rios
-$dir_logos = "C:\Drivers e Utilit·rios\Logos\" # Altere para o caminho de onde se encontram as logos
+$list_arq = @("imp_logo.bmp","Venda.bmp","Venda2.bmp") # Altere para os arquivos de logo necess√°rios
+$dir_logos = "C:\Drivers e Utilit√°rios\Logos\" # Altere para o caminho de onde se encontram as logos
 $qte_pontos = 15 # Altere para a quantidade de pontos existentes
 $qte_pontos++
 
@@ -17,9 +17,9 @@ for($l=0;$l-lt$list_arq.Length;$l++){
             $j = "00$i"
         }
             if((($i -ne 5) -and ($i -ne 7) -and ($i -ne 11) -and ($i -ne 14))){
-            #del ("C:\Chianca\NFE_Emissao$j\Danfe\imp") -Force
-            #mkdir ("C:\Chianca\NFE_Emissao$j\Danfe\imp") -Force
-            #mkdir ("C:\CHIANCA\0039$j\") -Force
+            # del ("C:\Chianca\NFE_Emissao$j\Danfe\imp") -Force
+            # mkdir ("C:\Chianca\NFE_Emissao$j\Danfe\imp") -Force
+            # mkdir ("C:\CHIANCA\0039$j\") -Force
             Rename-Item -Path "C:\CHIANCA\NFe_Emissao$j\Danfe\imp\Empresa.bmp" -NewName "C:\CHIANCA\NFe_Emissao$j\Danfe\imp\Empresa__old.bmp"
             Rename-Item -Path "C:\CHIANCA\0039$j\$($list_arq[$l])" -NewName "C:\CHIANCA\0039$j\$($list_arq[$l])__old.bmp"
             copy "$dir_logos$empresa" "C:\CHIANCA\NFe_Emissao$j\Danfe\imp\$empresa"
